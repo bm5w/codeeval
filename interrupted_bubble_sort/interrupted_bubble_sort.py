2d889ed2b730369b1ff77824a1415a3a26224f6d
Mark Saiget <saiget@gmail.com>
2015-05-30 15:18:42 -0700

    passes local tests, but not codeeval(timeout)

--- interrupted_bubble_sort/interrupted_bubble_sort.py
+++ interrupted_bubble_sort/interrupted_bubble_sort.py
@@ -5,18 +5,19 @@ https://www.codeeval.com/open_challenges/158/
 import sys
 
 
-def main(filename):
-    """Open file and perform bubble_sort on each line."""
-    with open(filename, 'r') as file_handle:
-        for line in file_handle:
-            interrupted_bs(line)
+def bubble_sort(inputL):
+    """One iteration of bubble sort."""
+    for num in xrange(len(inputL)-1):
+        if inputL[num+1] < inputL[num]:
+            inputL[num+1], inputL[num] = inputL[num], inputL[num+1]
+    return inputL
 
 
 def interrupted_bs(line):
     """Parse input line and preform bubble sort X times.
     X is defined as the integer after '|' on line."""
     temp = line.split('|')
-    num = int(temp[1])
+    num = int(temp[1].rstrip())
     temp[0] = temp[0].rstrip()
     inputL = [int(x) for x in temp[0].split(' ')]
     for x in xrange(num):
@@ -25,12 +26,11 @@ def interrupted_bs(line):
     print ' '.join(map(str, inputL))
 
 
-def bubble_sort(inputL):
-    """One iteration of bubble sort."""
-    for num in xrange(len(inputL)-1):
-        if inputL[num+1] < inputL[num]:
-            inputL[num+1], inputL[num] = inputL[num], inputL[num+1]
-    return inputL
+def main(filename):
+    """Open file and perform bubble_sort on each line."""
+    with open(filename, 'r') as file_handle:
+        for line in file_handle:
+            interrupted_bs(line)
 
 
 if __name__ == '__main__':
