2a5b25c14ae8fdc6f79414e5744c11508fa97674
Mark Saiget <saiget@gmail.com>
2015-06-02 16:52:36 -0700

    add _permutations method.

--- string_permutations/string_permutations.py
+++ string_permutations/string_permutations.py
@@ -5,3 +5,22 @@ Write a program which prints all the permutations of a string in alphabetical
 order. We consider that digits < upper case letters < lower case letters. The
 sorting should be performed in ascending order.
 """
+import sys
+from itertools import permutations
+
+
+def main(filename):
+    with open(filename, 'r') as file_handle:
+        for line in file_handle:
+            _permutations(line)
+
+
+def _permutations(line):
+    """Given input line, return all permutations in alphabetical order."""
+    output = permutations(line)
+    return output.join(',')
+
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    main()
--- string_permutations/test_string_permutations.py
+++ string_permutations/test_string_permutations.py
@@ -1,6 +1,6 @@
 from cStringIO import StringIO
 import sys
-import fizz_buzz
+import string_permutations
 
 
 class Capture(list):
@@ -8,13 +8,18 @@ class Capture(list):
     def __enter__(self):
         self._stdout = sys.stdout
         sys.stdout = self._stringio = StringIO()
+        return self
 
     def __exit__(self, *args):
         self.extend(self._stringio.getvalue().splitlines())
         sys.stdout = self._stdout
 
 
-
-expected = ['aht,ath,hat,hta,tah,tha'
-            'abc,acb,bac,bca,cab,cba',
-            '6Zu,6uZ,Z6u,Zu6,u6Z,uZ6']
+def test_string_premutations():
+    expected = ['aht,ath,hat,hta,tah,tha'
+                'abc,acb,bac,bca,cab,cba',
+                '6Zu,6uZ,Z6u,Zu6,u6Z,uZ6']
+    with Capture() as output:
+        string_permutations.main('input.txt')
+    for ex, act in map(none, expected, output):
+        assert ex == act
