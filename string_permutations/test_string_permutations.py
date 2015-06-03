3b2b610a911d55e5dcb90db3df1f2f688bb24b91
Mark Saiget <saiget@gmail.com>
2015-06-02 17:54:36 -0700

    remove capture context manager for capturing stdout.

--- string_permutations/test_string_permutations.py
+++ string_permutations/test_string_permutations.py
@@ -1,25 +1,11 @@
 from cStringIO import StringIO
-import sys
 import subprocess
-
-
-class Capture(list):
-    """Context manager for capturing stdout."""
-    def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = StringIO()
-        return self
-
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        sys.stdout = self._stdout
 
 
 def test_string_premutations():
     expected = ['aht,ath,hat,hta,tah,tha',
                 'abc,acb,bac,bca,cab,cba',
                 '6Zu,6uZ,Z6u,Zu6,u6Z,uZ6']
-    # with Capture() as output:
     process = subprocess.Popen('python string_permutations.py input.txt',
                                shell=True,
                                stdout=subprocess.PIPE)
