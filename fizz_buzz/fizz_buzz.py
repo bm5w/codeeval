ca7f8dcb48e3d45c5bfb3b4ca91d1293eb827844
Mark Saiget <saiget@gmail.com>
2015-06-03 16:34:41 -0700

    remove divisible code and inline function

--- fizz_buzz/fizz_buzz.py
+++ fizz_buzz/fizz_buzz.py
@@ -9,22 +9,17 @@ def main(filename):
             fizz_buzz(line)
 
 
-def divisible(num, denom):
-    """Return true if num is divisible by denom, false otherwise."""
-    return bool(num % denom == 0)
-
-
 def fizz_buzz(line):
     """Print correct fizz buzz output given input line."""
     inp = line.split(' ')
     x, y, num = int(inp[0]), int(inp[1]), int(inp[2])
     output = []
     for num in xrange(1, num+1):
-        if divisible(num, x) and divisible(num, y):
+        if bool(num % x == 0) and bool(num % y == 0):
             output.append('FB')
-        elif divisible(num, x):
+        elif bool(num % x == 0):
             output.append('F')
-        elif divisible(num, y):
+        elif bool(num % y == 0):
             output.append('B')
         else:
             output.append(str(num))
