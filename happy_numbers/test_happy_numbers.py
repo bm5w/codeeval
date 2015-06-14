02e2c8bee4760b6dad44dd54ea919a05300ab232
Mark Saiget <saiget@gmail.com>
2015-06-14 14:35:07 -0700

    Complete test_happy_numbers.py

--- happy_numbers/test_happy_numbers.py
+++ happy_numbers/test_happy_numbers.py
@@ -1,2 +1,12 @@
-expected = [1, 1, 0]
+import subprocess
 
+
+def test_happy_numbers():
+    expected = [1, 1, 0]
+    process = subprocess.Popen('python happy_numbers.py input.txt',
+                               shell=True,
+                               stdout=subprocess.PIPE)
+    output, stderr = process.communicate()
+    output = output.splitlines()
+    for ex, act in map(None, expected, output):
+        assert ex == act
