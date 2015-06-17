ff20a2946e3359879ea475b84e593db28e5aa991
Mark Saiget <saiget@gmail.com>
2015-06-16 22:32:41 -0700

    Add queue method and solution.

--- prefix_expressions/prefix_expressions.py
+++ prefix_expressions/prefix_expressions.py
@@ -1,44 +1,101 @@
-"""
-Solution for code eval problem:
-https://www.codeeval.com/open_challenges/7/
-
-PREFIX EXPRESSIONS
-CHALLENGE DESCRIPTION:
-
-You are given a prefix expression. Write a program which evaluates it.
-
-INPUT SAMPLE:
-
-Your program should accept a file as its first argument. The file contains one prefix expression per line.
-
-For example:
-
-
-1
-* + 2 3 4
-Your program should read this file and insert it into any data structure you like. Traverse this data structure and evaluate the prefix expression. Each token is delimited by a whitespace. You may assume that sum ‘+’, multiplication ‘*’ and division ‘/’ are the only valid operators appearing in the test data.
-
-OUTPUT SAMPLE:
-
-Print to stdout the output of the prefix expression, one per line.
-
-For example:
-
-
-1
-20
-CONSTRAINTS:
-
-The evaluation result will always be an integer ≥ 0.
-The number of the test cases is ≤ 40.
-"""
 from sys import argv
 
 
+class Node(object):
+    """A Node Class representing a Node in a queue.
+    Each node has a pointer to the next node and a value.
+    """
+
+    def __init__(self, value, back=None):
+        """Create Node with value and optional pointer.
+        If no pointer is specified, pointer is set to None.
+        """
+        self.point_previous = back
+        self.value = value
+
+
+class Queue(object):
+    """Class defining a queue data structure."""
+
+    def __init__(self):
+        """Create a queue with a pointer to the front and back of an empty queue
+        and set count to zero to keep track of size"""
+        self.front = None
+        self.back = None
+        self.count = 0
+
+    def enqueue(self, value):
+        """Adds a node with the value to the back of the queue"""
+        old_back = self.back
+        self.back = Node(value)
+        if self.size() > 0:  # if any Nodes: set back previous to current Node
+            old_back.point_previous = self.back
+        else:  # adding to an empty, than define front
+            self.front = self.back
+        self.count += 1
+
+    def dequeue(self):
+        """Returns a value of the front node and removes it from the queue
+        If the qpopulated_queueueue is empty, return AttributeError."""
+        try:
+            val = self.front.value
+            self.front = self.front.point_previous
+        except AttributeError:
+            raise AttributeError(u"Queue is empty")
+        self.count -= 1
+        return val
+
+    def peek(self):
+        """Returns a value of the front node"""
+        try:
+            return self.front.value
+        except AttributeError:
+            raise AttributeError(u"Queue is empty")
+
+    def size(self):
+        """Returns the size of the queue"""
+        return self.count
+
+
+def perform(operator, index, nums):
+    left = nums.dequeue()
+    for count in xrange(index):
+        nums.enqueue(nums.dequeue())
+    right = nums.dequeue()
+
+    if operator == '*':
+        new = left * right
+    elif operator == '/':
+        new = left / right
+    else:
+        new = left + right
+    nums.enqueue(new)
+    for remaining in xrange(nums.size()-1-index):
+        nums.enqueue(nums.dequeue())
+    return nums
+
 
 def prefix(line):
-    pass
+    nums_and_operators = line.split(' ')
+    nums = Queue()
+    operators = []
+    for x in nums_and_operators:
+        if ord(x) >= 48 and ord(x) <= 57:
+            nums.enqueue(int(x))
+        else:
+            operators.append(x)
+    possible_operators = [u'*', u'/', u'+']
 
+    for operator in possible_operators:
+        try:
+            index = operators.index(operator)
+            if index >= 0:
+                nums = perform(operator, index, nums)
+                operators.remove(operator)
+        except ValueError:
+            pass
+
+    return nums.peek()
 
 
 def main(input_file):
